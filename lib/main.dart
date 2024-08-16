e3f7ed397cae8a2d7ab15eb4aef048b1c16fc74d
ikramulhasantanvir <ikramulhasantanvir@gmail.com>
2024-08-16 19:19:18 +0600

    mediaQuery, layoutBuilder, device preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,94 +1,97 @@
-// Current SDK version: 3.22.1
-import 'package:flutter/cupertino.dart';
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
-import 'package:ostad_flutter_live_class/home.dart';
-import 'package:ostad_flutter_live_class/profile_page.dart';
-
-// TextField
 
 void main() {
-  runApp(IntoApp());
+  runApp(
+    DevicePreview(
+      enabled: true,
+      builder: (context) => MyApp(),
+    ),
+  );
 }
 
-class IntoApp extends StatelessWidget {
-  const IntoApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp(
-      debugShowCheckedModeBanner: true,
-      home: ProfilePage(),
-      // theme: ThemeData(
-      //   brightness: Brightness.light,
-      //   appBarTheme: AppBarTheme(
-      //       color: Colors.pink,
-      //       titleTextStyle: TextStyle(
-      //         color: Colors.white,
-      //         fontSize: 20,
-      //         fontWeight: FontWeight.bold,
-      //         letterSpacing: 2.0,
-      //       )),
-      //   textButtonTheme: TextButtonThemeData(
-      //     style: TextButton.styleFrom(foregroundColor: Colors.pink),
-      //   ),
-      //   inputDecorationTheme: InputDecorationTheme(
-      //     border: OutlineInputBorder(
-      //         borderRadius: BorderRadius.circular(8),
-      //         borderSide: BorderSide(color: Colors.red, width: 2)),
-      //     disabledBorder: OutlineInputBorder(
-      //       borderRadius: BorderRadius.circular(8),
-      //       borderSide: BorderSide(
-      //         color: Colors.green,
-      //         width: 2,
-      //       ),
-      //     ),
-      //     focusedBorder: OutlineInputBorder(
-      //       borderRadius: BorderRadius.circular(8),
-      //       borderSide: BorderSide(
-      //         color: Colors.yellow,
-      //         width: 2,
-      //       ),
-      //     ),
-      //   ),
-      //   textTheme: TextTheme(
-      //       titleLarge: TextStyle(
-      //     fontSize: 20,
-      //     fontWeight: FontWeight.bold,
-      //   )),
-      // ),
-      // darkTheme: ThemeData(
-      //   brightness: Brightness.dark,
-      //   appBarTheme: AppBarTheme(
-      //       color: Colors.pink,
-      //       titleTextStyle: TextStyle(
-      //         color: Colors.white,
-      //         fontSize: 20,
-      //         fontWeight: FontWeight.bold,
-      //         letterSpacing: 2.0,
-      //       )),
-      //   textButtonTheme: TextButtonThemeData(
-      //     style: TextButton.styleFrom(foregroundColor: Colors.pink),
-      //   ),
-      //   inputDecorationTheme: InputDecorationTheme(
-      //     border: OutlineInputBorder(
-      //       borderRadius: BorderRadius.circular(8),
-      //       borderSide: BorderSide(
-      //         width: 0.5,
-      //       ),
-      //     ),
-      //     disabledBorder: OutlineInputBorder(
-      //       borderRadius: BorderRadius.circular(8),
-      //     ),
-      //     focusedBorder: OutlineInputBorder(
-      //       borderRadius: BorderRadius.circular(8),
-      //       borderSide: BorderSide(
-      //         width: 0.5,
-      //         color: Colors.pink,
-      //       ),
-      //     ),
-      //   ),
-      // ),
-      // themeMode: ThemeMode.system,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //MediaQuery
+    MediaQueryData mediaQueryData = MediaQuery.of(context);
+    print('width ${mediaQueryData.size.width}');
+    print('height ${mediaQueryData.size.height}');
+    print('aspect ${mediaQueryData.size.aspectRatio}');
+    print('flipped ${mediaQueryData.size.flipped.height}');
+
+    print('display ${mediaQueryData.devicePixelRatio}');
+
+    if (mediaQueryData.size.width < 640) {
+      print('this is a Phone');
+    } else if (mediaQueryData.size.width > 640 &&
+        mediaQueryData.size.width < 1008) {
+      print('this is a table');
+    } else {
+      print('Laptop');
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('AppBar'),
+        backgroundColor: Colors.blue,
+      ),
+      // body: OrientationBuilder(builder: (context, Orientation orientation) {
+      //   print(orientation);
+      //   if (orientation == Orientation.portrait) {
+      //     return Column(
+      //       children: [
+      //         Wrap(
+      //           alignment: WrapAlignment.center,
+      //           crossAxisAlignment: WrapCrossAlignment.center,
+      //           // runAlignment: WrapAlignment.center,
+      //           spacing: 16,
+      //           children: [
+      //             Text('hy brother whats up?'),
+      //             Text('hy brother'),
+      //             Text('hy brother '),
+      //             Text('hy brother whats up?'),
+      //           ],
+      //         ),
+      //         Text(orientation.name),
+      //       ],
+      //     );
+      //   } else {
+      //     return Center(
+      //       child: Text('too big screen '),
+      //     );
+      //   }
+      // }),
+      body: LayoutBuilder(
+        builder: (context, BoxConstraints constraints) {
+          if (constraints.maxWidth < 640) {
+            return Text('this a phone');
+          } else if (constraints.maxWidth > 640 &&
+              constraints.maxWidth < 1008) {
+            return Text('this is a table');
+          } else {
+            return Text('this is a laptop');
+          }
+        },
+      ),
     );
   }
 }
